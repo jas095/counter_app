5999bfb200775165ac64117fc52c23c8c3ddf750
Jose Angel <jas@deb>
2020-07-30 22:59:02 -0400

    Initial configuration CounterApp

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Center(
+        child: Text('Nobody says it was easy'),
+      ),
+    );
+  }
+}
